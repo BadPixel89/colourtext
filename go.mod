74c2f9763d69264e404626871ca756b425b3601e
Dan <badpixel89@gmail.com>
2024-09-07 21:36:15 +0100

    added comments to each method that show the text added and output colour

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/BadPixel89/colourtext
 
 go 1.22.6
 
-require golang.org/x/sys v0.24.0 // indirect
+require golang.org/x/sys v0.24.0
--- main.go
+++ main.go
@@ -27,6 +27,8 @@ const White = "\033[97m"
 
 const _PARAMETER_IS_INCORRECT = 87
 
+//	enables console virtualisation in Windows, allowing PowerShell and CMD to output coloured text. 
+//	if this is not enabled, any output will have the colour characters prepended and appended to it
 func EnableVirtualConsole(console windows.Handle) error {
 	var mode uint32
 	err := windows.GetConsoleMode(console, &mode)
@@ -54,27 +56,35 @@ func PrintAny(colour string, v ...any) {
 	log.Printf(colour+"\n%+v"+Reset, v...)
 }
 
+//	prepend [pass] print in green
 func PrintSuccess(text string) {
 	loglnWithColour(Success+text, Green)
 }
+//	prepend [fail] print in red
 func PrintFail(text string) {
 	loglnWithColour(Fail+text, Red)
 }
+//	prepend [err ] print in red
 func PrintError(text string) {
 	loglnWithColour(Error+text, Red)
 }
+//	prepend [info] print in cyan
 func PrintInfo(text string) {
 	loglnWithColour(Info+text, Cyan)
 }
+//	prepend [warn] print in yellow
 func PrintWarn(text string) {
 	loglnWithColour(Warn+text, Yellow)
 }
+//	prepend [time] print in cyan
 func PrintTime(text string) {
 	loglnWithColour(Time+text, Cyan)
 }
+//	prepend [done] print in green
 func PrintDone(text string) {
 	loglnWithColour(Done+text, Green)
 }
+//	prepend [exit] print in red
 func PrintExit(text string) {
 	loglnWithColour(Exit+text, Red)
 }
